03a00fff8cc1b29d0168c00c7feee7c368f192bf
Александр Тимофеев <alextim@inbox.ru>
2021-11-29 15:11:03 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Lib.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestMailSender
+{
+    public class Library
+    {
+
+        public static bool Check(string login, string password)
+        {
+            if (login == "admin" && password == "admin") return true;
+            return false;
+        }
+
+        static public bool MailSend(string _from, string _to, string _subject, string _body, string _login, string _password, IEnumerable attachments = null)
+        {
+            bool result = false;
+            try
+            {
+                // отправитель - устанавливаем адрес и отображаемое в письме имя
+                MailAddress from = new MailAddress(_from);
+                // кому отправляем
+                MailAddress to = new MailAddress(_to);
+                // создаем объект сообщения
+                MailMessage m = new MailMessage(from, to);
+                // тема письма
+                m.Subject = _subject;
+                // текст письма
+                m.Body = _body;
+                // письмо представляет код html
+                m.IsBodyHtml = true;
+                //_password = System.IO.File.ReadAllText("C:\\temp\\1.txt");
+                if (attachments != null)
+                {
+
+                    foreach (object el in attachments)
+                    {
+                        System.Diagnostics.Debug.WriteLine(el.ToString());
+                        Attachment attachment = new Attachment(el.ToString());
+                        m.Attachments.Add(attachment);
+                    }
+                }
+                //if (_login.IndexOf('@') > 0) _login = _login.Remove(_login.IndexOf('@'));
+
+                // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                // логин и пароль
+                smtp.Credentials = new NetworkCredential(_login, _password);
+                smtp.EnableSsl = true;
+                smtp.Send(m);
+                result = true;
+            }
+            catch { }
+            
+            return result;
+        }
+    }
+}
---
+++ WpfApp2/WpfMailSender.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace WpfTestMailSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class WpfMailSender : Window
+    {
+        public WpfMailSender()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendMail_Click(object sender, RoutedEventArgs e)
+        {
+            string _password = System.IO.File.ReadAllText("C:\\temp\\2.txt");
+            if (Library.MailSend(tbxFrom.Text, tbxTo.Text, tbxTopic.Text, tbxMessage.Text, tbxFrom.Text, _password))
+            {
+                MessageBox.Show("Письмо успешно отправлено!");
+            }
+            else
+            {
+                MessageBox.Show("Ошибка отправки!");
+            }
+        }
+    }
+}
